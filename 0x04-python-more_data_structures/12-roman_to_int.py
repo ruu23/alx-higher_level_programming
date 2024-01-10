279790d228577bb765d26cf34eb41943c296fedc
ruu23 <arwamah777@gmail.com>
2024-01-10 22:38:51 +0000

    finally

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,7 +4,7 @@ def roman_to_int(roman_string):
     prev_value = 0
     if not isinstance(roman_string, str) or roman_string is None:
         return (0)
-    num = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M':1000}
+    num = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     for i in reversed(roman_string):
         value = num.get(i, 0)
 
