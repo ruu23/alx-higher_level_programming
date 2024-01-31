71c68779c89147705fe953e98630ce10121ca7e7
ruu23 <arwamah777@gmail.com>
2024-01-31 02:29:12 +0200

    advance

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    magic_string.x = getatt(magic_string, 'x', 0) + 1
+    magic_string.x = getattr(magic_string, 'x', 0) + 1
     return ", ".join(["BestSchool" for i in range (magic_string.x)])
