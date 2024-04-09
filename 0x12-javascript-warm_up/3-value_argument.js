14a487da3e7f3b243e3c01c839bd063df2ed81a3
ruu23 <arwamah777@gmail.com>
2024-04-09 04:51:35 +0200

    3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const count = process.argv.length;
 if (count === 2) {
-	console.log("No argument");
-}else if (count === 3) {
-	console.log(process.argv[2]);
+  console.log('No argument');
+} else if (count === 3) {
+  console.log(process.argv[2]);
 }
