2946d9645a0e473bf68e3304ef986b6b47b76529
ruu23 <arwamah777@gmail.com>
2024-04-10 03:44:42 +0200

    done

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -16,14 +16,15 @@ class Rectangle {
       console.log(s);
     }
   }
+
   rotate () {
-         [this.width, this.height] = [this.height, this.width];
- }
+    [this.width, this.height] = [this.height, this.width];
+  }
 
   double () {
-         this.width *= 2;
-         this.height *= 2;
- }
+    this.width *= 2;
+    this.height *= 2;
+  }
 }
 
 module.exports = Rectangle;
